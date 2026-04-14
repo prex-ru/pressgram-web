182e5740cf833a769f425cf47f00a3a01191386f
Florian Duros <florian.duros@ormaz.fr>
2026-04-14 10:17:38 +0200

    feat: disable custom section in vms

--- apps/web/src/viewmodels/room-list/RoomListItemViewModel.ts
+++ apps/web/src/viewmodels/room-list/RoomListItemViewModel.ts
@@ -303,6 +303,9 @@ export class RoomListItemViewModel
             canMarkAsRead,
             canMarkAsUnread,
             roomNotifState,
+            // To be implemented when custom section creation is added in vms
+            canCreateSection: false,
+            canMoveToSection: false,
         };
     }
 
@@ -380,5 +383,9 @@ export class RoomListItemViewModel
         // Set the notification state using EchoChamber
         const echoChamber = EchoChamber.forRoom(this.props.room);
         echoChamber.notificationVolume = elementNotifState;
+    };
+
+    public onCreateSection = (): void => {
+        // To be implemented when custom section creation is added in vms
     };
 }
